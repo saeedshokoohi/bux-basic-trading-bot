720bdf4d643ec3a79f96cea243107dba27b07868
shokoohi <saeed.shokuhi@gmail.com>
2021-09-30 17:23:10 +0330

    Initial source project commit

---
+++ src/main/java/com/bux/bot/basic_trading_bot/BasicTradingBotApplication.java
@@ -0,0 +1,13 @@
+package com.bux.bot.basic_trading_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTradingBotApplication {
+    //start point of app
+	public static void main(String[] args) {
+		SpringApplication.run(BasicTradingBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bux/bot/basic_trading_bot/BasicTradingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bux.bot.basic_trading_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicTradingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
